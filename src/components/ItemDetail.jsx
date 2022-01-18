c1b0896ee0fd6fe9c97f8e7e80e4592ca30e7ace
Alex <cnalex_2@hotmail.com>
2022-01-17 22:53:19 -0500

    tratando de sincronizar ItemCounter

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -4,9 +4,9 @@ export function ItemDetail({product}) {
   return(
     <div  className = "" >
       {JSON . stringify( product, null,  2 ) }
-      <div>
-      <ItemCount key={product.id} product={product} />
-      </div>
+      
+      <div><ItemCount/>tratando de sincronizar Itemcounter </div>
+      
      
     </div >
   )
